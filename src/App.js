84839c60c3a6df47d6219c8cf9a9b33cc870cacf
AnharAwadh <anhar9617@gmail.com>
2022-06-23 17:03:47 +0300

    Add hw day3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+
+  const [counterName, setCountryName] = useState(''); 
+  const [temperature, setTemperature] = useState('');
+  const [wind, setWind] = useState('');
+ 
+
+
+
+
+
+  useEffect (() => {
+    const Weather = async (e) => {
+      const request = await fetch( `https://goweather.herokuapp.com/weather/${counterName}`);
+      const data = await request.json();
+      setTemperature(data.temperature);
+      setWind(data.wind);};
+    
+      Weather();
+  }, [counterName]);
+
+
+
+
   return (
+    <>
+   
+   
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h4 className='text-center mt-3 mb-3 ml-60  w-100'>Weather App </h4>
+    <input onChange={(e) => setCountryName(e.target.value)} value={counterName} className="form-control" type="text" placeholder="Enter country name " />
+    <h4 className='text-center mt-3 ml-60  w-100'>The Weather in {' '} <span>{counterName}</span>  {""}</h4>
+    <h4 className='text-center mt-3 ml-60  w-100'> Temperature : {temperature} {""}</h4>
+    <h4 className='text-center mt-3 ml-60  w-100'> wind : {wind} {""} </h4>
+ 
+</div>
+
+    </>
   );
 }
 
